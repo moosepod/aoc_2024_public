d25f879d1c79418c78e6f3c928c8851ebb0c0332
Matthew Christensen <mchristensen@moosepod.com>
2024-12-03 06:52:14 -0500

    Day 3

--- utils.py
+++ utils.py
@@ -1,16 +1,3 @@
-def load(name):
-    import re
-    rx = re.compile(r'^(\d+) +(\d+)$')
-    
-    l1 = []
-    l2 = []
-
-    with open(f"day_1/{name}") as f:
-        for line in f.read().split("\n"):
-            if line:
-                m = rx.search(line)
-                s1,s2 = m.group(1), m.group(2)
-                l1.append(int(s1))
-                l2.append(int(s2))
-
-    return l1,l2
+def load_s(puzzle_number: int, filename: str) -> str:
+    with open(f"day_{puzzle_number}/{filename}") as f:
+        return f.read()
